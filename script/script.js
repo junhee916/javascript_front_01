c3a4b2f917fc61b5c538a4b476d89ca5e35ba2f8
junhee916 <junhee916@naver.com>
2021-08-03 11:40:55 +0900

    javascript_getElementById_test_01

--- script/script.js
+++ script/script.js
@@ -1,5 +1,46 @@
-var hw = document.getElementById('hw')
+/*
+* test 1
+*/
 
-hw.addEventListener('click', function(){
-    alert("Hello world")
+// var hw = document.getElementById('hw')
+
+// hw.addEventListener('click', function(){
+//     alert("Hello world")
+// })
+
+/*
+* test 2
+*/
+
+// const mode = document.getElementById("jsmode");
+
+// mode.addEventListener("click", function(){
+
+//     if(mode.innerText === "바뀌기 전 text"){
+//         mode.innerText = "바뀐 text!";
+//     }
+//     else { ("
+//         mode.innerText = "바뀌기 전 text";
+//     }
+// })
+
+const wbtn = document.getElementById("white_btn");
+wbtn.addEventListener("click", function() {
+
+    document.getElementById("target").className = "white"
+
+})
+
+const bbtn = document.getElementById("black_btn");
+bbtn.addEventListener("click", function(){
+
+    document.getElementById("target").className = "black"
+
+});
+
+const borderbtn = document.getElementById("border_btn");
+borderbtn.addEventListener("click", function() {
+
+    document.getElementById("target").classList.add("border");
+
 })
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use('/', static(path.join(__dirname, 'html')))
 app.use('/script', express.static(__dirname + '/script'))
+app.use(express.static('public'))
 
 const PORT = 5000 || 7000
 
